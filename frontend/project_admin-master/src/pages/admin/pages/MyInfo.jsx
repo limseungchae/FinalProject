07ec81912c19f317180f75805c4889ab4e84c17b
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-27 22:37:36 +0900

    cors 해제

--- frontend/project_admin-master/src/pages/admin/pages/MyInfo.jsx
+++ frontend/project_admin-master/src/pages/admin/pages/MyInfo.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useOutletContext} from "react-router-dom";
 import {RiUser5Line} from "react-icons/ri";
+import axios from 'axios';
 
 export default function MyInfo() {
     const isActive = useOutletContext();
+
+    // axios 테스트
+    const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/hello')
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error))
+    }, []);
 
     return (
         <div className={`adminMain px-5 ${isActive}`}>
@@ -16,6 +26,7 @@ export default function MyInfo() {
             <div id='adminContents'>
                 <p>이곳에서 post요청을 실험합니다.</p>
                 <p>데이터가 url + 쿼리스트링에 담겨 잘 전달되면 성공, 그렇지 않으면 실패입니다.</p>
+                백엔드에서 가져온 데이터입니다 : {hello}
             </div>
 
         </div>
--- src/main/java/com/example/finalproject/controller/FrontController.java
+++ src/main/java/com/example/finalproject/controller/FrontController.java
@@ -1,9 +1,14 @@
 package com.example.finalproject.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/")
-@CrossOrigin(origins = "https://localhost:3000")
+@RestController
 public class FrontController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
 }
