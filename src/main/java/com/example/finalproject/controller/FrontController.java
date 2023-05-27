fb16ddf71e08e8836b6b84889818d1c9aed7104e
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-27 21:41:47 +0900

    FrontController 생성

---
+++ src/main/java/com/example/finalproject/controller/FrontController.java
@@ -0,0 +1,9 @@
+package com.example.finalproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller("/")
+@CrossOrigin(origins = "https://localhost:3000")
+public class FrontController {
+}
