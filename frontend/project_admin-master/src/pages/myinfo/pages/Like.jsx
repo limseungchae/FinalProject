e320074ae76a875bd45a4abaa202f69085a40c83
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-31 17:47:56 +0900

    like 목록 proto 제작

--- frontend/project_admin-master/src/pages/myinfo/pages/Like.jsx
+++ frontend/project_admin-master/src/pages/myinfo/pages/Like.jsx
@@ -5,8 +5,19 @@ import Card from "react-bootstrap/Card";
 import {HiLocationMarker} from "react-icons/hi";
 import {ImStarEmpty, ImStarFull, ImStarHalf} from "react-icons/im";
 import {FaCarrot} from "react-icons/fa";
+import axios from "axios";
+import {MdOutlineSearchOff} from "react-icons/md";
 
 export default function Like(){
+    const [likeList, setLikeList] = useState([]);
+
+    useEffect(() => {
+        let param = ``;
+        axios.get(`http://localhost:8080/api/likey`)
+            .then(response => setLikeList(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return(
         <Container>
             <Row>
@@ -18,74 +29,28 @@ export default function Like(){
                         <hr />
                     </div>
                     <Row>
-                        <Col className="col-xl-4 mt-4">
-                            <Link to={""} style={{textDecoration:"none"}}>
-                                <Card border="light" className="mx-auto" style={{ width: '270px',color:"black" }}>
-                                    <Card.Img variant="top" src={"https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/6a43614b1f394a478e13cb7a13977485.jpg"} width="100%" height="218px" />
-                                    <Card.Body>
-                                        <div className="d-flex py-1">
-                                            <div><HiLocationMarker className="fs-5 fw-light" /> <span style={{fontSize:"12px"}}>광진구 / 서울</span></div>
-                                        </div>
-                                        <Card.Title className="fs-6 mb-0 fw-bold">[부산본점]메시아패스 자유수강권</Card.Title>
-                                        <Card.Text>
-                                            <p className="mb-0" ><span className="text-warning"><ImStarFull/> <ImStarFull/> <ImStarFull/> <ImStarHalf /> <ImStarEmpty /></span> <span className="fw-light" style={{fontSize:"12px"}}>(10)</span></p>
-                                            <p className="fw-bold text-end"><span className="text-danger">10% </span >10,000원</p>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Link>
-                        </Col>
-                        <Col className="col-xl-4 mt-4">
-                            <Link to={""} style={{textDecoration:"none"}}>
-                                <Card border="light" className="mx-auto" style={{ width: '270px',color:"black" }}>
-                                    <Card.Img variant="top" src={"https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/6a43614b1f394a478e13cb7a13977485.jpg"} width="100%" height="218px" />
-                                    <Card.Body>
-                                        <div className="d-flex py-1">
-                                            <div><HiLocationMarker className="fs-5 fw-light" /> <span style={{fontSize:"12px"}}>광진구 / 서울</span></div>
-                                        </div>
-                                        <Card.Title className="fs-6 mb-0 fw-bold">[부산본점]메시아패스 자유수강권</Card.Title>
-                                        <Card.Text>
-                                            <p className="mb-0" ><span className="text-warning"><ImStarFull/> <ImStarFull/> <ImStarFull/> <ImStarHalf /> <ImStarEmpty /></span> <span className="fw-light" style={{fontSize:"12px"}}>(10)</span></p>
-                                            <p className="fw-bold text-end"><span className="text-danger">10% </span >10,000원</p>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Link>
-                        </Col>
-                        <Col className="col-xl-4 mt-4">
-                            <Link to={""} style={{textDecoration:"none"}}>
-                                <Card border="light" className="mx-auto" style={{ width: '270px',color:"black" }}>
-                                    <Card.Img variant="top" src={"https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/6a43614b1f394a478e13cb7a13977485.jpg"} width="100%" height="218px" />
-                                    <Card.Body>
-                                        <div className="d-flex py-1">
-                                            <div><HiLocationMarker className="fs-5 fw-light" /> <span style={{fontSize:"12px"}}>광진구 / 서울</span></div>
-                                        </div>
-                                        <Card.Title className="fs-6 mb-0 fw-bold">[부산본점]메시아패스 자유수강권</Card.Title>
-                                        <Card.Text>
-                                            <p className="mb-0" ><span className="text-warning"><ImStarFull/> <ImStarFull/> <ImStarFull/> <ImStarHalf /> <ImStarEmpty /></span> <span className="fw-light" style={{fontSize:"12px"}}>(10)</span></p>
-                                            <p className="fw-bold text-end"><span className="text-danger">10% </span >10,000원</p>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Link>
-                        </Col>
-                        <Col className="col-xl-4 mt-4">
-                            <Link to={""} style={{textDecoration:"none"}}>
-                                <Card border="light" className="mx-auto" style={{ width: '270px',color:"black" }}>
-                                    <Card.Img variant="top" src={"https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/6a43614b1f394a478e13cb7a13977485.jpg"} width="100%" height="218px" />
-                                    <Card.Body>
-                                        <div className="d-flex py-1">
-                                            <div><HiLocationMarker className="fs-5 fw-light" /> <span style={{fontSize:"12px"}}>광진구 / 서울</span></div>
-                                        </div>
-                                        <Card.Title className="fs-6 mb-0 fw-bold">[부산본점]메시아패스 자유수강권</Card.Title>
-                                        <Card.Text>
-                                            <p className="mb-0" ><span className="text-warning"><ImStarFull/> <ImStarFull/> <ImStarFull/> <ImStarHalf /> <ImStarEmpty /></span> <span className="fw-light" style={{fontSize:"12px"}}>(10)</span></p>
-                                            <p className="fw-bold text-end"><span className="text-danger">10% </span >10,000원</p>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Link>
-                        </Col>
+                        { (likeList.length > 0) ? likeList.map((array) => {
+                            return(
+                                <Col className="col-xl-4 mt-4">
+                                    <Link to={`/viewclass?link=${array[0]}`} style={{textDecoration:"none"}}>
+                                        <Card border="light" className="mx-auto" style={{ width: '270px',color:"black" }}>
+                                            <Card.Img variant="top" src={array[9]} width="100%" height="218px" />
+                                            <Card.Body>
+                                                <div className="d-flex py-1">
+                                                    <div><HiLocationMarker className="fs-5 fw-light" /> <span style={{fontSize:"12px"}}>{array[3]} / {array[2]}</span></div>
+                                                </div>
+                                                <Card.Title className="fs-6 mb-0 fw-bold">{array[1]}</Card.Title>
+                                                <Card.Text>
+                                                    <p className="mb-0" ><span className="text-warning"><ImStarFull/> <ImStarFull/> <ImStarFull/> <ImStarHalf /> <ImStarEmpty /></span> <span className="fw-light" style={{fontSize:"12px"}}>({array[6]})</span></p>
+                                                    <p className="fw-bold text-end"><span className="text-danger">{Math.floor(array[7]*100)}% </span >{array[8].toLocaleString()}원</p>
+                                                </Card.Text>
+                                            </Card.Body>
+                                        </Card>
+                                    </Link>
+                                </Col>
+                            )
+                        }) : <Col className="col-xl-12 mt-4 text-center"><MdOutlineSearchOff style={{fontSize:"200px",color:"grey"}} /><p className="fw-bold h4">검색 결과가 없습니다</p></Col>
+                        }
                     </Row>
                 </Col>
             </Row>
