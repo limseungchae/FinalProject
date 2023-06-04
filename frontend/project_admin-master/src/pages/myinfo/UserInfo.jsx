a7c47da456b6a756936c261a74462b420880afcb
KIM CHUNG IL <cnddlf12@naver.com>
2023-06-04 17:50:44 +0900

    토큰 없을때, url로 /myinfo 이하의 경로로 들어가는 경우 제거(router 단에서 해결)

--- frontend/project_admin-master/src/pages/myinfo/UserInfo.jsx
+++ frontend/project_admin-master/src/pages/myinfo/UserInfo.jsx
@@ -6,16 +6,42 @@ import Header from "../front/component/Header";
 import Footer from "../front/component/Footer";
 import Footer_2 from "../front/component/Footer_2";
 import {MdOutlineKeyboardArrowRight} from "react-icons/md";
+import axios from "axios";
 
 export default function UserInfo() {
+    const token = localStorage.getItem("ACCESS_TOKEN");
+
+    const [userInfo, setUserInfo] = useState({});
     const [sidebarOpen, setSidebarOpen] = useState(false);
     const sidebarRef = useRef(null);
+
+// 유저 정보 요청
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                console.log(token)
+                const response = await axios.get("http://localhost:8080/test/user", {
+                    headers: {
+                        Authorization: `Bearer ${token}` // 토큰을 요청 헤더에 첨부합니다.
+                    }
+                });
+                console.log(response.data)
+                const userData = response.data;  // 응답에서 유저 정보 추출
+                setUserInfo(userData);           // 유저 정보를 userInfo에 저장
+            } catch (error) {
+                console.error("유저 정보 요청 실패:", error);
+            }
+        };
+
+        fetchUserInfo().then(r => r);
+    }, [token]);
+
+    console.log(userInfo);
+
 
     const toggleSidebar = () => {
         setSidebarOpen(prevState => !prevState);
     };
-
-
 
     useEffect(() => {
         const handleWheelEvent = (event) => {
@@ -35,7 +61,6 @@ export default function UserInfo() {
             }
         };
     }, [sidebarOpen]);
-
 
     return (
         <>
