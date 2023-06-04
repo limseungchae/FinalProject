40df43e856edffd89a135eed858c219a92c8c9ea
KIM CHUNG IL <cnddlf12@naver.com>
2023-06-05 01:30:20 +0900

    /search 헤더 수정

--- frontend/project_admin-master/src/pages/search/component/SearchBar.jsx
+++ frontend/project_admin-master/src/pages/search/component/SearchBar.jsx
@@ -1,28 +1,70 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./SearchBar.css"
-import {Button, Col, Container, Form, Navbar, Row} from "react-bootstrap";
+import {Button, Col, Container, Navbar, Row} from "react-bootstrap";
 import Dropdown from 'react-bootstrap/Dropdown';
-import InputGroup from 'react-bootstrap/InputGroup';
-import logo4 from "../img/logo4.png";
 
-import {Link} from "react-router-dom";
-import { BsFillHeartFill } from "react-icons/bs";
+import {Link, useNavigate} from "react-router-dom";
 import { BsFillPersonFill } from "react-icons/bs";
 import { BsSearch } from "react-icons/bs";
 
 // logo 테스트
 export default function SearchBar({search, setSearch, processSearch}) {
+    const navigate = useNavigate();
+    const [token, setToken] = useState("null");
+
+    useEffect(()=> {
+        setToken(localStorage.getItem("ACCESS_TOKEN"));
+    }, [])
+
+
+    /* // 로컬 스토리지에서 ACCESS TOKEN 가져오기
+     const accessToken = localStorage.getItem("ACCESS_TOKEN");*/
+
+    const [isMyMenuOpen, setIsMyMenuOpen] = useState(false);
+
+    /*    const handleDropdown = () => {
+            if (localStorage.getItem("ACCESS_TOKEN") == null) {
+                window.location.href = "/login";
+            }
+        };*/
+
+    const handleMyMenuToggle = () => {
+        setIsMyMenuOpen(!isMyMenuOpen);
+    };
+
+    const closeMyMenu = () => {
+        setIsMyMenuOpen(false);
+    };
+
+
+    const logout = () => {
+        localStorage.setItem("ACCESS_TOKEN", null);
+        setToken("null")
+        window.location.href = "/";
+    }
+
+    const login = () => {
+        window.location.href = "/login";
+    }
+
+    const handleLike = () => {
+        const tokenExists = (token !== "null");
+        (!tokenExists) ? navigate('/login') : navigate("/myinfo/like");
+    };
+
+    const handlePayList = () => {
+        const tokenExists = (token !== "null");
+        (!tokenExists) ? navigate('/login') : navigate("/myinfo/paylist");
+    };
 
     return (
       <header className={'header-container'}>
           <Navbar className="headerNav" expand="lg">
               <Container>
-                  <Row className="w-100 align-items-center">
+                  <Row className="w-100 align-items-center" style={{boxShadow:"rgba(0, 0, 0, 0.07) 0px 2px 0px 0px"}}>
                       <Col lg={1} className="offset-2">
                           <div className="logo-container d-flex justify-content-between" style={{marginLeft:"-10px"}}>
-                              <Link to="/">
-                                <img src={logo4} alt="logo4" style={{padding:"8px"}}/>
-                              </Link>
+                              <Link href to="/" className='navbar-brand text-white logo' ><span style={{color:"#F7B400"}}>S</span><span style={{color:"#00C2AC"}}>R</span></Link>
                           </div>
                       </Col>
 
@@ -39,23 +81,57 @@ export default function SearchBar({search, setSearch, processSearch}) {
                           </div>
                       </Col>
 
-                      <Col lg={2} className="d-flex justify-content-end">
-                          <div className="d-flex headerLink mt-2" style={{marginRight:"80px"}}>
-                              <Link to="/myinfo/like" className="toCartPg">
-                                  <div className="icon-text-container">
-                                      <BsFillHeartFill />
-                                      <span className="icon-text">찜</span>
-                                  </div>
-                              </Link>
-                              <div className="icon-text-container">
-                                  <BsFillPersonFill />
-                                  <span className="icon-text">마이</span>
-                              </div>
+                      <Col lg={1} className="d-flex justify-content-end">
+                          <div className="d-ex headerLink mt-2fl">
+                              <Dropdown
+                                  onMouseEnter={handleMyMenuToggle}
+                                  onMouseLeave={closeMyMenu}
+                                  show={isMyMenuOpen}>
+                                  <Dropdown.Toggle
+                                      as={CustomToggle}
+                                      id="dropdown-my-menu"
+                                      className="my-button">
+                                      <BsFillPersonFill className="my-icon" />
+                                      <span className="icon-text my-text" >마이</span>
+                                  </Dropdown.Toggle>
+                                  <Dropdown.Menu>
+                                      <Dropdown.Item onClick={handleLike}>찜목록</Dropdown.Item>
+                                      <Dropdown.Item onClick={handlePayList}>결재 내역</Dropdown.Item>
+                                  </Dropdown.Menu>
+                              </Dropdown>
                           </div>
                       </Col>
+
+                      {(token !== "null")
+                          ?
+                          <Col lg={2} className={'d-flex justify-content-end'}>
+                              <div className={'d-flex mt-2'}>
+                                  <Button className={'btn btn-dark'} onClick={logout}>로그아웃</Button>
+                              </div>
+                          </Col>
+                          :
+                          <Col lg={2} className={'d-flex justify-content-end'}>
+                              <div className={'d-flex mt-2'}>
+                                  <Button className={'btn btn-secondary'} onClick={login}>로그인</Button>
+                              </div>
+                          </Col>
+                      }
+
                   </Row>
               </Container>
           </Navbar>
       </header>
   );
 }
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <div
+        ref={ref}
+        onClick={(e) => {
+            e.preventDefault();
+            onClick(e);
+        }}
+    >
+        {children}
+    </div>
+));
