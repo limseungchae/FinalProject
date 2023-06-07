99eeb7cd4ebc26b7012bcf884baddf1d88fc9b93
KIM CHUNG IL <cnddlf12@naver.com>
2023-06-08 02:50:36 +0900

    /paylist 필터 구현, 테이블 디자인 변경 []디자인 변경

--- frontend/project_admin-master/src/pages/myinfo/pages/Paylist.jsx
+++ frontend/project_admin-master/src/pages/myinfo/pages/Paylist.jsx
@@ -5,11 +5,14 @@ import * as classmeta from "react-bootstrap/ElementChildren";
 import axios from "axios";
 import {FaCarrot} from "react-icons/fa";
 import {useOutletContext} from "react-router-dom";
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 export default function Paylist() {
     /*const [status, setStatus] = useState('');*/
     const userInfo = useOutletContext();
-    const [payList, setPayList] = useState([]);
+    const [rows, setRows] = useState([{
+        id:0
+    }]);
     const kId = userInfo.kakaoid;
 
 
@@ -22,13 +25,78 @@ export default function Paylist() {
             .catch(console.log)
             .then(response => {
                 console.log(response?.data);
-                setPayList(response?.data);
+
+                if(response?.data !== null) {
+                    let data = response?.data;
+                    let rows = []
+                    // id, cname, tuter, quantity, totprice, paydate, tid
+                    for(let i = 0; data?.length > i; i++) {
+                        let row = {
+                            id: data[i].rno,
+                            cname: data[i].cname,
+                            tuter: "없음",
+                            actdate: data[i].actdate,
+                            quantity: data[i].quantity,
+                            totprice: data[i].totprice,
+                            paydate: data[i].paydate?.split('T')[0],
+                            tid: (data[i].tid !== null) ? "결제성공" : "예약확인",
+                        }
+                        rows.push(row)
+                    }
+                    setRows(rows)
+                }
             });
     }, [kId])
 
     /*const handleCancel = () => {
         setStatus('예약취소');
     };*/
+
+    const columns = [
+        { field: 'cname', headerName: '클래스', width: 420, headerAlign: 'center' },
+        { field: 'tuter', headerName: '강사명', width: 100 },
+        { field: 'actdate', headerName: '일정', width: 100 },
+        {
+            field: 'quantity',
+            headerName: '인원',
+            width: 100,
+        },
+        {
+            field: 'totprice',
+            headerName: '가격',
+            /*description: 'This column has a value getter and is not sortable.',*/
+            /*sortable: false,*/
+            width: 100,
+            valueFormatter: (params) => {
+                const formattedPrice = new Intl.NumberFormat('en-US').format(params.value);
+                return `${formattedPrice}원`;
+            },
+            /*valueGetter: (params) =>
+                `${params.row.firstName || ''} ${params.row.lastName || ''}`,*/
+        },
+        {
+            field: 'paydate',
+            headerName: '결제일자',
+            width: 130,
+        },
+        {
+            field: 'tid',
+            headerName: '결제상태',
+            width: 80,
+            renderCell: (params) => {
+                const tid = params.value;
+                let cellStyle = {};
+
+                if (tid === '결제성공') {
+                    cellStyle = { backgroundColor: 'blue', color: 'white' };
+                } else if (tid === '예약확인') {
+                    cellStyle = { backgroundColor: 'red', color: 'white' };
+                }
+
+                return <div style={cellStyle}>{tid}</div>;
+            },
+        }
+    ];
 
 
     return (
@@ -37,8 +105,15 @@ export default function Paylist() {
                 <h3><FaCarrot className={"mb-2"} style={{color:"#00C2AC"}}/> 결재내역</h3>
                 <hr />
             </div>
-            <Table striped bordered hover responsive variant="primary">
-                <thead>
+            <div style={{ height: "100%", width: '100%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSizeOptions={[5, 10]}
+                />
+            </div>
+            {/*<Table striped bordered hover responsive>
+                <thead >
                 <tr>
                     <th>클래스명</th>
                     <th>강사명</th>
@@ -51,8 +126,8 @@ export default function Paylist() {
                 </thead>
                 <tbody>
                 {
-                    (payList.length > 0) ?
-                        payList.map(list => {
+                    (rows?.length > 0) ?
+                        rows?.map(list => {
                             return (
                                <tr>
                                    <td>{list.cname}</td>
@@ -61,32 +136,14 @@ export default function Paylist() {
                                    <td>{list.quantity}</td>
                                    <td>{list.totprice}</td>
                                    {(list.paydate !== null) ? <td>{list.paydate}</td> : <td></td>}
-                                   {(list.tid !== null) ? <td>결제완료</td> : <td>예약완료</td> }
+                                   {(list.tid !== null) ? <td>결제완료</td> : <td>예약성공</td> }
                                </tr>
                             )
                         }) : <tr></tr>
                 }
-
-
-                {/*{(cla => (
-                    <tr>
-                        <td>1111</td>
-                        <td>1111</td>
-                        <td>2023-05-30</td>
-                        <td>1111</td>
-                        <td>1111</td>
-                        <td>2023-05-30</td>
-                        <td>
-                            {status ? (
-                                '예약취소'
-                            ) : (
-                                <button onClick={handleCancel}>취소</button>
-                            )}
-                        </td>
-                    </tr>
-                ))}*/}
                 </tbody>
-            </Table>
+            </Table>*/}
         </div>
     );
 }
+
