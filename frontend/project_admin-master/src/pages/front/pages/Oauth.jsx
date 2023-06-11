902c67e3b6a21300f7c500ebcc5769e97298785e
hideonbush3 <ppoii0961@gmail.com>
2023-06-11 16:56:11 +0900

    로그인 성공시 localStorage에 kakaoid 저장

--- frontend/project_admin-master/src/pages/front/pages/Oauth.jsx
+++ frontend/project_admin-master/src/pages/front/pages/Oauth.jsx
@@ -27,6 +27,8 @@ export default function Oauth() {
                 .then((res)=> {
                 if(res.data.token) {
                     localStorage.setItem("ACCESS_TOKEN", res.data.token);
+                    localStorage.setItem("kakaoid", res.data.kakaoid);
+                    console.log(res.data);
                     // token이 존재하는 경우 메인페이지로 리디렉트
                     window.location.href = '/';
                 }
