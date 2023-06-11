0e223dc62c89443c6489b605feac205e5d0145c7
KIM CHUNG IL <cnddlf12@naver.com>
2023-06-11 21:46:55 +0900

    테스트 파일 삭제

--- src/test/java/com/example/finalproject/FrontTest.java
+++ src/test/java/com/example/finalproject/FrontTest.java
@@ -13,49 +13,4 @@ import java.util.List;
 
 @SpringBootTest
 public class FrontTest {
-    @Autowired
-    FrontRepository frontRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
-    public String kId = "2813856259";
-
-
-    @Test
-    @DisplayName("all")
-    public void findAll() {
-        List<ClassMeta> cms = frontRepository.findAll();
-
-        for(ClassMeta cm: cms)
-            System.out.println(cm);
-    }
-
-    @Test
-    @DisplayName("all")
-    public void findMain() {
-        List<Object[]> cms = frontRepository.findMainAll();
-
-        for(Object[] cm: cms)
-            System.out.println(cm);
-    }
-
-    @Test
-    @DisplayName("like")
-    public void findlike() {
-        List<Object[]> lks = frontRepository.findLikeyByUserid(kId);
-
-        for(Object[] lk: lks)
-            System.out.println(lk);
-    }
-
-    @Test
-    @DisplayName("test")
-    public void chk() {
-        User chk = userRepository.findUserByKakaoid("2813856259");
-
-        System.out.println(chk);
-    }
-
-
 }
